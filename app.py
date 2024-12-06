f7ee4d5bc945edd715358ace2784a5026a940feb
Tungify <jollyut@gmail.com>
2024-12-06 23:25:52 +0700

    execute web run

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ health_status = {
 async def get_user_and_cookies():
     if not api.sessions:
         await api.create_sessions(
-            headless=False,
+            headless=True,
             ms_tokens=[ms_token],
             num_sessions=1,
             sleep_after=300,
