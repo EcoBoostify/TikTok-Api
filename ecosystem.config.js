8f03c263eb77472c2e267a676d62b6c91cd596ea
Tungify <jollyut@gmail.com>
2024-12-09 13:02:08 +0700

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
     apps: [
         {
-            name: 'python-app',     // Tên ứng dụng
-            script: 'xvfb-run',     // Chạy xvfb-run
-            args: 'python3 /path/to/your/venv/bin/app.py',  // Thêm đường dẫn đúng đến script Python
-            interpreter: '/path/to/your/venv/bin/python3', // Sử dụng Python trong virtual environment
-            instances: 1,           // Chạy 1 instance (hoặc nhiều nếu cần)
-            autorestart: true,      // Tự động khởi động lại khi ứng dụng gặp sự cố
-            watch: false,           // Không theo dõi thay đổi mã nguồn
-            max_memory_restart: '1G',  // Restart nếu sử dụng bộ nhớ vượt quá 1GB
+            name: 'python-app',   // Tên ứng dụng
+            script: 'xvfb-run',   // Chạy xvfb-run
+            args: 'python3 app.py', // Thêm lệnh chạy ứng dụng Python
+            interpreter: 'bash',  // Dùng bash để chạy lệnh
+            instances: 1,         // Số lượng instance, nếu bạn muốn chạy nhiều tiến trình thì chỉnh sửa ở đây
+            autorestart: true,    // Tự động khởi động lại nếu ứng dụng gặp sự cố
+            watch: false,         // Không theo dõi thay đổi mã nguồn
+            max_memory_restart: '1G',  // Restart nếu tiêu thụ bộ nhớ vượt quá 1GB
         }
     ]
 };
